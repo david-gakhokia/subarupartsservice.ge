592b032efcb5fd62fdd6349bdd4e36f363df6d5f
Davit Gakhokia <david.gakhokia@gmail.com>
2025-05-18 03:41:08 +0400

    Update iframe source URL in form view to point to the correct lead form

--- resources/views/front/form.blade.php
+++ resources/views/front/form.blade.php
@@ -5,7 +5,7 @@
         <div class="container">
 
 
-            <iframe src="https://leadsmardige-main-8dkl12.laravel.cloud/sms-form" width="100%" height="800"
+            <iframe src="https://leadsmardige-main-8dkl12.laravel.cloud/lead-form" width="100%" height="800"
                 frameborder="0" style="border: none; overflow: hidden;" scrolling="no">
             </iframe>
 
